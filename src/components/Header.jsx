7baa5cad6cf77ec3a09ea7acff86ce7a08a223c5
Daniel <daniel.40.pompa@gmail.com>
2023-05-02 22:19:36 +0200

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <div>
+      <h1>
+        Clínica Veterinaria {''}
+        <span className='main-color'>React</span>
+      </h1>
+    </div>
+  );
+};
+
+export default Header;
