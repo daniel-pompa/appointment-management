844f7ac331eecdddd77232aacd6c489603701b92
Daniel <daniel.40.pompa@gmail.com>
2023-05-02 23:11:30 +0200

    Create the form fields

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -8,6 +8,104 @@ const Form = () => {
         Formulario nuevas {''}
         <span className='main-color'>citas</span>
       </p>
+
+      {/* Form */}
+      <form className='bg-white shadow-lg rounded-md mx-3 p-4'>
+        <div className='my-4'>
+          <label htmlFor='pet' className='block text-gray-600 font-bold'>
+            Nombre Mascota
+          </label>
+          <input
+            id='pet'
+            type='text'
+            placeholder='Nombre de la mascota'
+            className='w-full text-gray-500 border-2 p-1 mt-1 rounded-md'
+          />
+        </div>
+
+        <div className='my-4'>
+          <label htmlFor='owner' className='block text-gray-600 font-bold'>
+            Nombre Propietario
+          </label>
+          <input
+            id='owner'
+            type='text'
+            placeholder='Nombre del propietario'
+            className='w-full text-gray-500 border-2 p-1 mt-1 rounded-md'
+          />
+        </div>
+
+        <div className='my-4'>
+          <label htmlFor='phone' className='block text-gray-600 font-bold'>
+            Teléfono
+          </label>
+          <input
+            id='phone'
+            type='tel'
+            placeholder='Teléfono de contacto'
+            pattern='[0-9]{3} [0-9]{2} [0-9]{2} [0-9]{2}'
+            className='w-full text-gray-500 border-2 p-1 mt-1 rounded-md'
+          />
+          <small className='text-gray-600 font-bold'>
+            Formato: 648 45 67 89
+          </small>
+        </div>
+
+        <div className='my-4'>
+          <label htmlFor='email' className='block text-gray-600 font-bold'>
+            Correo electrónico
+          </label>
+          <input
+            id='email'
+            type='email'
+            placeholder='Correo electrónico de contacto'
+            className='w-full text-gray-500 border-2 p-1 mt-1 rounded-md'
+          />
+        </div>
+
+        <div className='my-4'>
+          <label htmlFor='date' className='block text-gray-600 font-bold'>
+            Fecha
+          </label>
+          <input
+            id='date'
+            type='date'
+            className='w-full text-gray-500 border-2 p-1 mt-1 rounded-md'
+          />
+        </div>
+
+        <div className='my-4'>
+          <label htmlFor='time' className='block text-gray-600 font-bold'>
+            Hora
+          </label>
+          <input
+            id='time'
+            type='time'
+            min='09:00'
+            max='18:00'
+            className='w-full text-gray-500 border-2 p-1 mt-1 rounded-md'
+          />
+          <small className='text-gray-600 font-bold'>
+            Horario 9:00 a 18:00
+          </small>
+        </div>
+
+        <div className='my-4'>
+          <label htmlFor='symptoms' className='block text-gray-600 font-bold'>
+            Síntomas
+          </label>
+          <textarea
+            id='symptoms'
+            placeholder='Descripción de los síntomas'
+            className='w-full text-gray-500 border-2 p-1 mt-1 rounded-md'
+          />
+        </div>
+
+        <input
+          type='submit'
+          className='w-full text-sm text-white font-bold uppercase hover:bg-sky-400 cursor-pointer transition-all rounded-md mb-3 p-3 main-background-color'
+        />
+      </form>
     </div>
   );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <div className='flex justify-center flex-col md:flex-row'>
-      <h1 className='text-4xl text-center text-gray-600 font-black uppercase mt-16 mb-4'>
+      <h1 className='text-4xl text-center text-gray-600 font-black uppercase mt-12 mb-4'>
         Clínica Veterinaria {''}
         <span className='main-color'>React</span>
       </h1>
