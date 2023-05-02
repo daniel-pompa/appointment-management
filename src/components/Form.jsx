61b9b254cc9628c7be2aa47548e269bdf8e84273
Daniel <daniel.40.pompa@gmail.com>
2023-05-02 22:30:33 +0200

    Create Form and PatientList components

---
+++ src/components/Form.jsx
@@ -0,0 +1,13 @@
+const Form = () => {
+  return (
+    <div>
+      <h2>Citas Pacientes</h2>
+      <p>
+        Formulario nuevas {''}
+        <span className='main-color'>citas</span>
+      </p>
+    </div>
+  );
+};
+
+export default Form;
---
+++ src/components/PatientList.jsx
@@ -0,0 +1,13 @@
+const PatientList = () => {
+  return (
+    <div>
+      <h2>Lista Pacientes</h2>
+      <p>
+        Administrar citas {''}
+        <span className='main-color'>Pacientes</span>
+      </p>
+    </div>
+  );
+};
+
+export default PatientList;
