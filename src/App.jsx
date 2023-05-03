d6ee190a2f012c98bab1d2bbb6360583a919b419
Daniel <daniel.40.pompa@gmail.com>
2023-05-04 00:56:25 +0200

    Data persistence in LocalStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Form from './components/Form';
 import Header from './components/Header';
 import PatientList from './components/PatientList';
 
 function App() {
   /* Create state of patients
-  It starts as an empty array and is filled with the data entered in the form */
-  const [patients, setPatients] = useState([]);
+  Starts with patients stored in localStorage or with an empty array and will be filled with the data entered in the form */
+  const [patients, setPatients] = useState(
+    JSON.parse(localStorage.getItem('patients')) || []
+  );
 
   /* Create the patient state
   It starts as an empty object
   When clicking the edit button of Patient.jsx component will populate the object */
   const [patient, setPatient] = useState({});
+
+  // Patients useEffect
+  useEffect(() => {
+    localStorage.setItem('patients', JSON.stringify(patients));
+  }, [patients]);
 
   // Function to delete a patient's appointment
   const deletePatient = id => {
