edd6d8ada7822f9ba1aff1bec00c87705c237a61
Daniel <daniel.40.pompa@gmail.com>
2023-05-03 23:44:59 +0200

    Conditional display of text in the patient list

--- src/components/PatientList.jsx
+++ src/components/PatientList.jsx
@@ -5,13 +5,27 @@ const PatientList = ({ patients }) => {
   return (
     <div className='md:w-1/2 lg:w-3/5 my-8 md:h-screen custom-scrollbar custom-scrollbar-firefox'>
       <div className='sticky top-0 bg-gray-100'>
-        <h2 className='text-2xl text-gray-600 text-center font-black uppercase'>
-          Lista Pacientes
-        </h2>
-        <p className='text-sm text-center text-gray-600 font-bold uppercase mt-2 pb-4'>
-          Administrar citas {''}
-          <span className='main-color'>Pacientes</span>
-        </p>
+        {patients && patients.length ? (
+          <>
+            <h2 className='text-2xl text-gray-600 text-center font-black uppercase'>
+              Lista Pacientes
+            </h2>
+            <p className='text-sm text-center text-gray-600 font-bold uppercase mt-2 pb-4'>
+              Administrar citas {''}
+              <span className='main-color'>Pacientes</span>
+            </p>
+          </>
+        ) : (
+          <>
+            <h2 className='text-2xl text-gray-600 text-center font-black uppercase'>
+              No hay citas
+            </h2>
+            <p className='text-sm text-center text-gray-600 font-bold uppercase mt-2'>
+              Crea una nueva {''}
+              <span className='main-color'>Cita</span>
+            </p>
+          </>
+        )}
       </div>
 
       {/* Iterate patient array and display in patient list */}
